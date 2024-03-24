8c7b522c5430081b2ae011c89a61ea16ed6828dc
Piotr Puchała <260421@student.pwr.edu.pl>
2024-03-24 21:11:32 +0100

    Create basic game board

---
+++ frontend/src/components/BoardField.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps<{
+  index: number;
+  state: string;
+  isWinning: boolean;
+}>()
+
+const classes = computed<string>(() => {
+  const fieldClasses = []
+  if (props.index < 6) fieldClasses.push('border-bottom')
+  if (props.index > 2) fieldClasses.push('border-top')
+  if (props.index % 3 != 2) fieldClasses.push('border-right')
+  if (props.index % 3 != 0) fieldClasses.push('border-left')
+
+  return fieldClasses.reduce((acc, e) => `${acc} ${e}`) + ` field ${props.state == '0' && 'empty-field'}`
+})
+
+const image = computed<string>(() => {
+  return props.state == '1' ? '/x-symbol.svg' : '/o-symbol.svg'
+})
+</script>
+
+<template>
+  <div :class="classes">
+    <img v-if="state != '0'" :src="image" alt="player symbol" class="symbol"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$border-width: 7px;
+
+.field {
+  width: 250px;
+  height: 250px;
+  border-style: solid;
+  border-color: #232121;
+  border-width: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.empty-field:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+}
+
+.border-top {
+  border-top-width: $border-width;
+}
+
+.border-bottom {
+  border-bottom-width: $border-width;
+}
+
+.border-right {
+  border-right-width: $border-width;
+}
+
+.border-left {
+  border-left-width: $border-width;
+}
+
+.symbol {
+  width: 80%;
+  height: 80%;
+}
+</style>
--- frontend/src/views/BoardView.vue
+++ frontend/src/views/BoardView.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
+import {useGame} from "@/composables/useGame";
+import BoardField from "@/components/BoardField.vue";
 
+const {game} = useGame()
+
+const testState: string[] = ['0', '1', '2', '1', '2', '0', '0', '0', '0']
 </script>
 
 <template>
-  <div>playing :)</div>
+  <div class="board">
+    <BoardField
+        v-for="(state, index) in testState"
+        :key="index"
+        :is-winning="false"
+        :index="index"
+        :state="state"
+    />
+  </div>
 </template>
 
 <style scoped>
-
+.board {
+  width: fit-content;
+  justify-items: center;
+  display: grid;
+  grid-template-columns: repeat(3, 0.33fr);
+  grid-template-rows: repeat(3, 0.33fr);
+  margin: auto;
+}
 </style>
