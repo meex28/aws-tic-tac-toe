e53412bccf0d9d38410e2e242e516cbb17776b06
Piotr Puchała <260421@student.pwr.edu.pl>
2024-05-20 20:02:11 +0200

    Add CallbackView styling

--- frontend/src/views/CallbackView.vue
+++ frontend/src/views/CallbackView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import {onMounted} from "vue";
 import {useCognitoAuth} from "@/composables/useCognitoAuth";
+import Spinner from "@/components/core/Spinner.vue";
+import CoreDialog from "@/components/core/CoreDialog.vue";
 
 const {onCallback} = useCognitoAuth();
 
@@ -10,11 +12,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Callback</h1>
-  </div>
+  <CoreDialog>
+    <div class="container">
+      <h1>Authorizing</h1>
+      <Spinner/>
+    </div>
+  </CoreDialog>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
 
+  h1 {
+    color: white;
+  }
+}
 </style>
